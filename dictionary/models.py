d2a5fe242921fb431e1ed4097c97679c8ef04654
Mazen Alalwan <mazen13998@gmail.com>
2019-03-23 21:53:55 +0300

    synonyms
    made the synonyms much more effecient

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from nltk.corpus import wordnet
+from nltk.stem import LancasterStemmer
+lancaster = LancasterStemmer()
 
 
 class Word(models.Model):
     symbol = models.ImageField(upload_to='images/')
     main = models.CharField(max_length=20)
 
+    def synonyms(self):
+        synonyms = []
+        for syn in wordnet.synsets(self.main):
+
+            for l in syn.lemmas():
+                synonyms.append(l.name())
+        synonyms = sorted(set(synonyms[0:3]))
+        return synonyms
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
-import nltk
-from nltk.corpus import wordnet
 from .models import Word
 
 
 def home(request):
     words = Word.objects
-
-    dictionary = {}
-
-    for word in words.all():
-        synonyms = []
-        for syn in wordnet.synsets(word.main):
-
-            for l in syn.lemmas():
-                synonyms.append(l.name())
-        dictionary[word.main] = set(synonyms)
-
-    return render(request, "dictionary/home.html", {'words': words, 'dictionary': dictionary.items()})
+    return render(request, "dictionary/home.html", {'words': words})
 
